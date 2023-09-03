a6559e6ed1ac864e3be841a8a91dcffe8832ca9e
youssefalatrash <104606746+youssefalatrash@users.noreply.github.com>
2023-09-03 13:27:07 +0300

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProduct';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+
+
+export{
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+}
